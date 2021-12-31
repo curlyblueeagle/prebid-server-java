3c425dfb89ee8c0a49901d13745a7173a71ff44f
markiian <mmykush@magnite.com>
2021-12-31 15:57:48 +0200

    Consolidate Unruly Adapter: Refactor

--- src/test/java/org/prebid/server/it/UnrulyTest.java
+++ src/test/java/org/prebid/server/it/UnrulyTest.java
@@ -22,8 +22,12 @@ public class UnrulyTest extends IntegrationTest {
     public void openrtb2AuctionShouldRespondWithBidsFromUnruly() throws IOException, JSONException {
         // given
         WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/unruly-exchange"))
-                .withRequestBody(equalToJson(jsonFrom("openrtb2/unruly/test-unruly-bid-request.json")))
-                .willReturn(aResponse().withBody(jsonFrom("openrtb2/unruly/test-unruly-bid-response.json"))));
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/unruly/test-unruly-bid-request-1.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/unruly/test-unruly-bid-response-1.json"))));
+
+        WIRE_MOCK_RULE.stubFor(post(urlPathEqualTo("/unruly-exchange"))
+                .withRequestBody(equalToJson(jsonFrom("openrtb2/unruly/test-unruly-bid-request-2.json")))
+                .willReturn(aResponse().withBody(jsonFrom("openrtb2/unruly/test-unruly-bid-response-2.json"))));
 
         // when
         final Response response = responseFor("openrtb2/unruly/test-auction-unruly-request.json",
